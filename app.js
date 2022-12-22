e52dd8aeb32b01e7569914a3df4a18c71213d84c
Habilu-Abakuri <info2habilu@gmail.com>
2022-12-22 14:44:48 +0100

    final commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+/* get the elements for DOM manipulation*/
 const sound = document.getElementById("audioSound");
 const result = document.getElementById("displayResult");
 const searchWord = document.getElementById("searchButton");
@@ -13,6 +14,7 @@ searchWord.addEventListener("click", function(e){
         return;
     }
 
+        /* call the inputword function to fetch the word definition, usage and sound*/
     search_function(inputWord)
     
 });
@@ -21,7 +23,7 @@ async function search_function(inputWord){
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`);
     
     if (!response.ok){
-        /*throw new Error(`HTTP error! status: ${response.status}`);*/
+        throw new Error(`HTTP error! status: ${response.status}`);
         alert("word not in dictionary")
     }
     const data = await response.json();
@@ -46,15 +48,19 @@ async function search_function(inputWord){
         ${data[0].meanings[0].definitions[0].example}
     </p>
     `;
+
+    /* call the sound attribute to get the audio data from the api*/
     sound.getAttribute("src", `https:${data[0].meanings[0].
     definitions[0].audio}`);
     
+    /* call the sound function to play the sound of the word*/
     function playAudio(){
         sound.play();
     };
     
 };
 
+/* sound function */
 function playAudio(){
     sound.play();
 };
